2ae2cab8bf50b1f9b34bbd9fee6f78b5e79d496b
mzielu <kmarcinzielinski@gmail.com>
2016-08-24 21:40:41 +0200

    AND-1 Submodules created  * .gitignore files added  * AppBase and BaseActivity created

---
+++ app/src/main/java/com/activity_sync/App.java
@@ -0,0 +1,16 @@
+package com.activity_sync;
+
+public class App extends AppBase
+{
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate()
+    {
+        super.onTerminate();
+    }
+}
