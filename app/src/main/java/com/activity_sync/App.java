69dd4ab00cfd6bc5165e46576590315c1a92c0c7
mzielu <kmarcinzielinski@gmail.com>
2016-08-24 21:50:32 +0200

    AND-1 Screen and Presenter created  * build.gradle files updated

--- app/src/main/java/com/activity_sync/App.java
+++ app/src/main/java/com/activity_sync/App.java
@@ -1,16 +1,26 @@
 package com.activity_sync;
 
+import com.activity_sync.di.DiComponent;
+import timber.log.Timber;
+
 public class App extends AppBase
 {
     @Override
     public void onCreate()
     {
         super.onCreate();
+
+        initializeLogging();
     }
 
     @Override
     public void onTerminate()
     {
         super.onTerminate();
+    }
+
+    private void initializeLogging()
+    {
+        Timber.plant(new Timber.DebugTree());
     }
 }
