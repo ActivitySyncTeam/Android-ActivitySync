60ff741478c7500002a77d1af3824843e0380330
mzielu <kmarcinzielinski@gmail.com>
2016-08-25 01:08:54 +0200

    AND-1 Dummy Screen implemented

--- presentation/src/main/java/com/activity_sync/presentation/views/IDummyView.java
+++ presentation/src/main/java/com/activity_sync/presentation/views/IDummyView.java
@@ -1,5 +1,12 @@
 package com.activity_sync.presentation.views;
 
+import rx.Observable;
+
 public interface IDummyView
 {
+    Observable displayMessageClick();
+
+    Observable openDummyScreenClick();
+
+    void displayMessage();
 }
