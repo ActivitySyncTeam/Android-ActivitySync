4fbd9af85de12d29390d73699a220a9763269c23
mzielu <kmarcinzielinski@gmail.com>
2016-08-28 05:28:13 +0200

    AND-4 onDoneClick and onSkipClicked behaviour added

--- app/src/main/java/com/activity_sync/screens/IntroScreen.java
+++ app/src/main/java/com/activity_sync/screens/IntroScreen.java
@@ -1,13 +1,22 @@
 package com.activity_sync.screens;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import com.activity_sync.presentation.services.INavigator;
+import com.activity_sync.services.Navigator;
 import com.github.paolorotolo.appintro.AppIntro2;
 
 public class IntroScreen extends AppIntro2
 {
+    private INavigator navigator;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
+        navigator = new Navigator(this);
 
         addSlide(new IntroWelcomeScreen());
         addSlide(new IntroHappyScreen());
@@ -15,5 +24,19 @@ public class IntroScreen extends AppIntro2
         addSlide(new IntroSimpleScreen());
 
         setColorTransitionsEnabled(true);
+    }
+
+    @Override
+    public void onDonePressed(Fragment currentFragment)
+    {
+        super.onDonePressed(currentFragment);
+        navigator.openDummyScreen();
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment)
+    {
+        super.onSkipPressed(currentFragment);
+        navigator.openDummyScreen();
     }
 }
